dcd28a0e28563e76103a0f5b877d52050100494f
Daniil Silin <daniil_silin@mail.ru>
2023-02-27 16:25:43 +0000

    NEWUI updated track indicator

--- thesis_mobile/lib/view/new_ui/screens/track_order_screen.dart
+++ thesis_mobile/lib/view/new_ui/screens/track_order_screen.dart
@@ -26,16 +26,16 @@ class TrackOrderScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Order'),
+        title: const Text('Order'),
         actions: [
           GestureDetector(
             onTap: () => showModalBottomSheet(
               context: context,
               isScrollControlled: true,
-              builder: (context) => OrderContact(),
+              builder: (context) => const OrderContact(),
             ),
-            child: Padding(
-              padding: const EdgeInsets.only(right: 20),
+            child: const Padding(
+              padding: EdgeInsets.only(right: 20),
               child: Icon(Icons.dangerous),
             ),
           ),
@@ -50,78 +50,73 @@ class TrackOrderScreen extends StatelessWidget {
               height: MediaQuery.of(context).size.height * 0.45,
               alignment: Alignment.center,
               decoration: BoxDecoration(
-                color: AppColors.Cloud,
+                color: AppColors.cloud,
                 borderRadius: BorderRadius.circular(34),
                 image: DecorationImage(
-                    image: AssetImage('assets/testmap.png'),
+                    image: const AssetImage('assets/testmap.png'),
                     fit: BoxFit.cover,
                     colorFilter: ColorFilter.mode(
                         Colors.black.withOpacity(0.5), BlendMode.srcOver)),
-                boxShadow: [
+                boxShadow: const [
                   BoxShadow(
                       color: Color.fromRGBO(17, 54, 41, 0.1), blurRadius: 10)
                 ],
               ),
               child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
-                    color: AppColors.Dorian,
+                    color: AppColors.dorian,
                     borderRadius: BorderRadius.circular(34),
                   ),
-                  child: Text(
+                  child: const Text(
                     'Map tracking unavailable',
                     textAlign: TextAlign.center,
-                    style: TextStyle(color: AppColors.Graphite, fontSize: 14),
+                    style: TextStyle(color: AppColors.graphite, fontSize: 14),
                   )),
             ),
             Container(
               margin: const EdgeInsets.only(top: 20, bottom: 10),
               child: Row(
                 children: [
-                  Text('${DateFormat('hh:mm').format(date.toLocal())}',
-                      style: TextStyle(
+                  Text(DateFormat('hh:mm').format(date.toLocal()),
+                      style: const TextStyle(
                           fontSize: 28,
                           fontWeight: FontWeight.w700,
-                          color: AppColors.Graphite)),
-                  SizedBox(
+                          color: AppColors.graphite)),
+                  const SizedBox(
                     width: 13,
                   ),
-                  Text('Waiting time')
+                  const Text('Waiting time')
                 ],
               ),
             ),
-            //Добавить шаги
             if (order.status != OrderStatus.rejected)
               Container(
                 margin: const EdgeInsets.only(bottom: 20),
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      //todo fix indicator wtih 3 states
                       OrderStepIndicator(
-                        isCompleted: order.status != OrderStatus.waitingPayment,
+                        isCompleted: order.status.index >
+                            OrderStatus.waitingPayment.index,
                         isCurrent: order.status == OrderStatus.waitingPayment,
                       ),
                       OrderStepIndicator(
-                        isCompleted:
-                            order.status != OrderStatus.waitingWarehouse &&
-                                order.status != OrderStatus.waitingPayment,
+                        isCompleted: order.status.index >
+                            OrderStatus.waitingWarehouse.index,
                         isCurrent: order.status == OrderStatus.waitingWarehouse,
                       ),
                       OrderStepIndicator(
-                        isCompleted:
-                            order.status != OrderStatus.acceptedWarehouse &&
-                                order.status != OrderStatus.waitingPayment &&
-                                order.status != OrderStatus.waitingWarehouse,
+                        isCompleted: order.status.index >
+                            OrderStatus.acceptedWarehouse.index,
                         isCurrent:
                             order.status == OrderStatus.acceptedWarehouse,
                       ),
                       OrderStepIndicator(
-                        isCompleted: order.status == OrderStatus.delivered &&
-                            order.status != OrderStatus.acceptedWarehouse &&
-                            order.status != OrderStatus.waitingPayment &&
-                            order.status != OrderStatus.waitingWarehouse,
+                        isCompleted:
+                            order.status.index > OrderStatus.delivering.index,
                         isCurrent: order.status == OrderStatus.delivering,
                       ),
                     ]),
@@ -132,20 +127,20 @@ class TrackOrderScreen extends StatelessWidget {
                 height: 9,
                 child: ClipRRect(
                     borderRadius: BorderRadius.circular(16),
-                    child: LinearProgressIndicator(
+                    child: const LinearProgressIndicator(
                       value: 100,
-                      color: AppColors.LightRed,
-                      backgroundColor: AppColors.Dorian,
+                      color: AppColors.lightRed,
+                      backgroundColor: AppColors.dorian,
                     )),
               ),
             Container(
               alignment: Alignment.centerLeft,
               child: Text(
                 orderStatusParser(order.status),
-                style: TextStyle(
+                style: const TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w500,
-                    color: AppColors.Graphite),
+                    color: AppColors.graphite),
               ),
             ),
             Container(
@@ -153,7 +148,7 @@ class TrackOrderScreen extends StatelessWidget {
                 margin: const EdgeInsets.only(top: 10),
                 child: Text(
                   'Order № ${order.id}',
-                  style: TextStyle(fontSize: 16),
+                  style: const TextStyle(fontSize: 16),
                 )),
           ],
         ),
