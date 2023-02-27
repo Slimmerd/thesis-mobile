9782d3df8483999910a4ccf435de6791d34c5676
Daniil Silin <daniil_silin@mail.ru>
2023-02-27 15:49:43 +0000

    Added page push and remove method

--- thesis_mobile/lib/utils/custom_page_push.dart
+++ thesis_mobile/lib/utils/custom_page_push.dart
@@ -20,3 +20,25 @@ Future<dynamic> customPagePush(context, screen) => Navigator.of(context).push(
         },
       ),
     );
+
+Future<dynamic> customPagePushRemove(context, screen) =>
+    Navigator.of(context).pushAndRemoveUntil(
+        PageRouteBuilder(
+          pageBuilder: (BuildContext context, Animation<double> animation,
+                  Animation<double> secondaryAnimation) =>
+              screen,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(0.0, 1.0);
+            const end = Offset.zero;
+            const curve = Curves.ease;
+
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+            return SlideTransition(
+              position: animation.drive(tween),
+              child: child,
+            );
+          },
+        ),
+        (Route<dynamic> route) => false);
