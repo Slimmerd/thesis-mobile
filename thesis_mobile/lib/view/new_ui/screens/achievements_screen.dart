3b5746cb0839ca7abdbe8c89e4465b4d2b364575
Daniil Silin <daniil_silin@mail.ru>
2023-02-27 16:24:38 +0000

    NEWUI fixed error with constant update of achievements list. Added locked section.

--- thesis_mobile/lib/view/new_ui/screens/achievements_screen.dart
+++ thesis_mobile/lib/view/new_ui/screens/achievements_screen.dart
@@ -6,7 +6,22 @@ import 'package:thesis_mobile/core/bloc/statistics/statistics_bloc.dart';
 import 'package:thesis_mobile/core/bloc/task_manager/task_manager_bloc.dart';
 import 'package:thesis_mobile/view/new_ui/widgets/achievement_card.dart';
 
-class AchievementsScreen extends StatelessWidget {
+class AchievementsScreen extends StatefulWidget {
+  const AchievementsScreen({super.key});
+
+  @override
+  State<AchievementsScreen> createState() => _AchievementsScreenState();
+}
+
+class _AchievementsScreenState extends State<AchievementsScreen> {
+  @override
+  void initState() {
+    final statsContext = BlocProvider.of<StatisticsBloc>(context);
+    final achievementContext = BlocProvider.of<AchievementsBloc>(context);
+    achievementContext.updateAchievement(statsContext.state);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final taskContext = BlocProvider.of<TaskManagerBloc>(context);
@@ -14,14 +29,15 @@ class AchievementsScreen extends StatelessWidget {
 
     return BlocBuilder<AchievementsBloc, AchievementsState>(
       builder: (context, state) {
-        final statsContext = BlocProvider.of<StatisticsBloc>(context);
-        AchievementsBloc ctx = context.read<AchievementsBloc>();
-        ctx.updateAchievement(statsContext.state);
         return Scaffold(
-          appBar: AppBar(title: Text('Achievements')),
+          appBar: AppBar(title: const Text('Achievements')),
           body: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               children: [
+                Text(
+                  'Completed',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
                 Container(
                   margin: const EdgeInsets.symmetric(vertical: 20.0),
                   child: Wrap(
@@ -30,6 +46,25 @@ class AchievementsScreen extends StatelessWidget {
                     runSpacing: 10.h,
                     children: state.achieved
                         .map((e) => AchievementCard(
+                              image: e.image,
+                              title: e.title,
+                            ))
+                        .toList(),
+                  ),
+                ),
+                Text(
+                  'Locked',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Wrap(
+                    direction: Axis.horizontal,
+                    spacing: 10.w,
+                    runSpacing: 10.h,
+                    children: state.notAchieved
+                        .map((e) => AchievementCard(
+                              achieved: false,
                               image: e.image,
                               title: e.title,
                             ))
