91a1346b45f42625da8f7577ca66e6f19ce8663f
Daniil Silin <daniil_silin@mail.ru>
2023-02-27 15:49:09 +0000

    Updated namings

--- thesis_mobile/lib/utils/typography.dart
+++ thesis_mobile/lib/utils/typography.dart
@@ -1,52 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'colors.dart';
 
 class NewTypography {
-  static final DefaultText14 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 14, fontWeight: FontWeight.w400);
-  static final DefaultText12 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 12, fontWeight: FontWeight.w500);
-  static final DefaultText10 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 10, fontWeight: FontWeight.w400);
+  static final defaultText14 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 14, fontWeight: FontWeight.w400);
+  static final defaultText12 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 12, fontWeight: FontWeight.w500);
+  static final defaultText10 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 10, fontWeight: FontWeight.w400);
 
-  static final Header24 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 24, fontWeight: FontWeight.w700);
-  static final Header16 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 16, fontWeight: FontWeight.w700);
-  static final Header14 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 14, fontWeight: FontWeight.w700);
-  static final Header12 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 12, fontWeight: FontWeight.w500);
+  static final header24 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 24, fontWeight: FontWeight.w700);
+  static final header16 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 16, fontWeight: FontWeight.w700);
+  static final header14 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 14, fontWeight: FontWeight.w700);
+  static final header12 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 12, fontWeight: FontWeight.w500);
 
 // Montserrat
-  static final M12400 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 12, fontWeight: FontWeight.w400);
+  static final m12400 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 12, fontWeight: FontWeight.w400);
 
-  static final M18600 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 18, fontWeight: FontWeight.w600);
+  static final m18600 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 18, fontWeight: FontWeight.w600);
 
-  static final M16400 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 16, fontWeight: FontWeight.w400);
+  static final m16400 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 16, fontWeight: FontWeight.w400);
 
-  static final M14400 = GoogleFonts.montserrat(
-      color: AppColors.Graphite, fontSize: 14, fontWeight: FontWeight.w400);
+  static final m14400 = GoogleFonts.montserrat(
+      color: AppColors.graphite, fontSize: 14, fontWeight: FontWeight.w400);
 
 // Roboto
 
-  static final R12700 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 12, fontWeight: FontWeight.w700);
+  static final r12700 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 12, fontWeight: FontWeight.w700);
 
-  static final R12600 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 12, fontWeight: FontWeight.w600);
-  static final R12400 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 12, fontWeight: FontWeight.w400);
+  static final r12600 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 12, fontWeight: FontWeight.w600);
+  static final r12400 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 12, fontWeight: FontWeight.w400);
 
-  static final R16700 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 16, fontWeight: FontWeight.w700);
+  static final r16700 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 16, fontWeight: FontWeight.w700);
 
-  static final R10500 = GoogleFonts.roboto(
-      color: AppColors.Graphite, fontSize: 10, fontWeight: FontWeight.w500);
+  static final r10500 = GoogleFonts.roboto(
+      color: AppColors.graphite, fontSize: 10, fontWeight: FontWeight.w500);
 }
