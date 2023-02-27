9d5ab831ac14aa11427514a67298b875118c6017
Daniil Silin <daniil_silin@mail.ru>
2023-02-27 16:25:22 +0000

    NEWUI added package info

--- thesis_mobile/lib/view/new_ui/screens/menu_screen.dart
+++ thesis_mobile/lib/view/new_ui/screens/menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:thesis_mobile/core/bloc/task_manager/task_manager_bloc.dart';
 import 'package:thesis_mobile/utils/colors.dart';
 import 'package:thesis_mobile/utils/custom_page_push.dart';
@@ -8,8 +9,37 @@ import 'package:thesis_mobile/view/new_ui/screens/achievements_screen.dart';
 import 'package:thesis_mobile/view/new_ui/screens/orders_screen.dart';
 import 'package:thesis_mobile/view/new_ui/screens/statistics_screen.dart';
 import 'package:thesis_mobile/view/new_ui/widgets/stage_card.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class MenuScreen extends StatelessWidget {
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final taskContext = BlocProvider.of<TaskManagerBloc>(context);
@@ -17,47 +47,65 @@ class MenuScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(padding: EdgeInsets.all(20), children: [
-        StageCard(),
-        Divider(),
+      body: ListView(padding: const EdgeInsets.all(20), children: [
+        const StageCard(),
+        const Divider(),
         ListTile(
-          leading: Icon(Icons.shopping_bag),
-          onTap: () => customPagePush(context, OrdersScreen()),
-          title: Text('Orders'),
+          leading: const Icon(Icons.shopping_bag),
+          onTap: () => customPagePush(context, const OrdersScreen()),
+          title: Text(
+            'Orders',
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
         ),
         ListTile(
-          leading: Icon(Icons.forest),
-          onTap: () => customPagePush(context, StatisticsScreen()),
-          title: Text('Statistics'),
+          leading: const Icon(Icons.forest),
+          onTap: () => customPagePush(context, const StatisticsScreen()),
+          title: Text(
+            'Statistics',
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
         ),
         ListTile(
-          leading: Icon(Icons.star),
-          onTap: () => customPagePush(context, AchievementsScreen()),
-          title: Text('Achievements'),
+          leading: const Icon(Icons.star),
+          onTap: () => customPagePush(context, const AchievementsScreen()),
+          title: Text(
+            'Achievements',
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
         ),
-        SizedBox(
+        const SizedBox(
           height: 20,
         ),
         ElevatedButton(
             onPressed: () => showModalBottomSheet(
                   context: context,
                   isScrollControlled: true,
-                  builder: (context) => OrderContact(),
+                  builder: (context) => const OrderContact(),
                 ),
             style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.Dorian,
+              backgroundColor: AppColors.dorian,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(34.0),
               ),
               elevation: 0,
-              minimumSize: Size(335, 53),
+              minimumSize: const Size(335, 53),
             ),
-            child: Text('Contact us',
-                style: TextStyle(fontSize: 18, color: AppColors.Graphite))),
-        SizedBox(
+            child: const Text('Contact us',
+                style: TextStyle(fontSize: 18, color: AppColors.graphite))),
+        const SizedBox(
           height: 20,
         ),
-        TextButton(onPressed: () {}, child: Text('Rules and agreements')),
+        //todo
+        TextButton(
+            onPressed: () => launchUrlString(''),
+            child: const Text('Rules and agreements')),
+        const SizedBox(
+          height: 20,
+        ),
+        Center(
+            child: Text(
+                'Version ${_packageInfo.version} (${_packageInfo.buildNumber})')),
       ]),
     );
   }
